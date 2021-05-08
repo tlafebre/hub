c1cf49504e828e4c60fc8208996a6a87963ff555
Tjeerd Lafebre <tjeerd.lafebre@ing.com>
2021-05-08 09:13:27 +0200

    all repos are cloned into a directory

--- src/main.rs
+++ src/main.rs
@@ -15,17 +15,17 @@ struct GitRepo {
 
 fn get_api_token(location: String) -> Result<String, String> {
     if let Ok(token) = fs::read_to_string(location) {
-        return Ok(token);
+        Ok(token)
     } else {
         Err(String::from("failed to read token"))
     }
 }
 
-fn git_clone(repo: Value, destination: &str) {
-    //return Result
+fn git_clone(repo: Value) {
+    let url = repo["clone_url"].as_str().unwrap();
     Command::new("git")
         .arg("clone")
-        .arg(repo["clone_url"].as_str().unwrap())
+        .arg(url)
         .status()
         .expect("failed to clone");
 }
@@ -36,10 +36,10 @@ fn http_get(url: String) -> HttpResult<blocking::Response> {
     client.get(url).header(USER_AGENT, "my rust program").send()
 }
 
-fn mkdir_p(path: &str) -> Result<(&str), std::io::Error> {
+fn mkdir_p(path: &str) -> Result<&str, std::io::Error> {
     fs::create_dir_all(path)?;
 
-    Ok((path))
+    Ok(path)
 }
 
 fn get_repo_list() -> String {
@@ -57,13 +57,17 @@ fn get_repo_list() -> String {
 
 fn main() {
     let repo_list = get_repo_list();
-    let Ok(repo_dir) = mkdir_p("repos");
+    let repo_dir = mkdir_p("repos").unwrap();
 
     let v: Vec<Value> = serde_json::from_str(&repo_list).unwrap();
 
-    for o in v.into_iter() {
-        if &o["language"] == "Rust" {
-            git_clone(o, repo_dir)
+    if std::env::set_current_dir(repo_dir).is_ok() {
+        for o in v.into_iter() {
+            if &o["language"] == "Rust" {
+                git_clone(o)
+            }
         }
+    } else {
+        eprint!("Something went wrong");
     }
 }
